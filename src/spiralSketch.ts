fab7b44e3547aaa421225774af4b20941e4d85f5
Maksim Vasilyev <m.vasilyev@sweedpos.com>
2025-08-15 20:28:27 +0400

    spiral v2

--- src/spiralSketch.ts
+++ src/spiralSketch.ts
@@ -3,12 +3,11 @@ import p5 from "p5";
 const sketch = (p: p5) => {
   const WIDTH = window.innerWidth,
     HEIGHT = window.innerHeight,
-    ANGULAR_SPEED = 1 / 70,
-    BRUSH_SPEED = 1 / 90;
+    // ANGULAR_SPEED = 1,
+    ANGULAR_SPEED = 2,
+    BRUSH_SPEED = 10;
 
-  let time = 0,
-    angle = 0,
-    d = 4;
+  let time = 0;
 
   p.setup = () => {
     p.createCanvas(WIDTH, HEIGHT);
@@ -19,25 +18,44 @@ const sketch = (p: p5) => {
     p.angleMode("degrees");
   };
 
-  p.draw = () => {
-    time += 1;
-    angle += time * ANGULAR_SPEED;
-    d += time * BRUSH_SPEED;
+  function getNodes(): [number, number][] {
+    return Array.from({ length: 2000 }, (_, i) => {
+      return [i * BRUSH_SPEED, i * ANGULAR_SPEED * p.sin(p.frameCount)];
+    });
+  }
 
-    if (d > WIDTH) return;
-
-    const x = d * p.cos(angle);
-    const y = d * p.sin(angle);
+  function drawNode([d, angle]: [number, number], i: number, maxI: number) {
+    const adelta = p.frameCount * 1.5;
+    const x = d * p.cos(angle + adelta);
+    const y = d * p.sin(angle + adelta);
     const color = p.lerpColor(
-      p.color("rgba(146, 5, 165, 1)"),
-      p.color("rgba(52, 9, 152, 1)"),
-      (time % 600) / 600
+      p.color("rgba(103, 3, 116, 1)"),
+      p.color("rgba(45, 1, 147, 1)"),
+      p.sin(p.frameCount * 1 + (i / maxI) * 4 * 360)
     );
     p.fill(color);
+    p.circle(WIDTH / 2 + x, HEIGHT / 2 + y, d);
+  }
 
-    p.translate(WIDTH / 2 + x, HEIGHT / 2 + y);
-    p.circle(0, 0, d);
+  p.draw = () => {
+    p.background("black");
+    const nodes = getNodes();
+    nodes.forEach((x, i, arr) => drawNode(x, i, arr.length));
   };
+  //   if (d > WIDTH) return;
+
+  //   const x = d * p.cos(angle);
+  //   const y = d * p.sin(angle);
+  //   const color = p.lerpColor(
+  //     p.color("rgba(146, 5, 165, 1)"),
+  //     p.color("rgba(52, 9, 152, 1)"),
+  //     (time % 600) / 600
+  //   );
+  //   p.fill(color);
+
+  //   p.translate(WIDTH / 2 + x, HEIGHT / 2 + y);
+  //   p.circle(0, 0, d);
+  // };
 };
 
 export default sketch;
